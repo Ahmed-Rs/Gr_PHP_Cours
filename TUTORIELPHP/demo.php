869c194065d20157c82fd1700fce1182f4fff782
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-12-23 11:35:43 +0100

    Les variables

---
+++ TUTORIELPHP/demo.php
@@ -0,0 +1,21 @@
+Hi all !
+<?php
+
+    // // Les guillemets doubles interprètent les variables qu'ils entourent, contrairement aux guillemets simple qui ne font qu'afficher
+    // $nom = 'Marc';
+    // $prenom = 'Doe';
+    // echo "$nom\n$prenom";
+
+    // Exercice
+    $prenom = 'Marc';
+    $nom = 'Doe';
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = ($note1 + $note2)/2;
+    
+    echo "Bonjour $prenom $nom, vous avez eu $moyenne de moyenne.";
+
+
+?>
+
+How are you!
