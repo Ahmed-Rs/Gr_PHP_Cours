cfbd530be22d7a2b98ccc8d8867df6e11ac569f5
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-12-25 21:35:43 +0100

    Les fonctions utilisateurs

--- TUTORIELPHP/demo.php
+++ TUTORIELPHP/demo.php
@@ -1,63 +1,65 @@
 <?php
 
-/* $mot = readline("Veuillez entre un mot: ");
-$mot = strtolower($mot);
-$reverse = strrev($mot);
+// declare(strict_types=1); Ne eprmet pas la conversion implicite des params.
 
-if ($mot === $reverse) {
-    echo "Ce mot est un palindrome.";
-} else {
-    echo "Ce mot n'est pas un palindrome.";
-}
- */
+function repondre_oui_non (string $phrase): bool {   
+    while (true) {
 
-/* $notes = [10, 18, 19];
-$notes2 &= $notes; // Ici on passe une référence, la modification d'une variable implique la modif de la seconde variable. (Fct mutable)
-array_push($notes, 16, 17); // Cette fct modifie la variable $notes, Présence du & dans la description php.net 
-$notesReversed = array_reverse($notes); // Cette fct ne modifie pas la valeur de sa variable, d'où la nécessité de la stocker dans une variable. (Fct imutable)
-print_r($notes);
-$somme = array_sum($notes);
-$nbreElements = count($notes);
-$moyenne = round($somme / $nbreElements);
-echo "Votre moyenne est de " . $moyenne .".";
+        $reponse = readline($phrase . "(o/n): ");
+        
+        if ($reponse === 'o') {
+            return true;
 
- */
-
-/* while (true) {
-    $mot = readline("Entrez votre mot : \n");
-    if ($mot === '') {
-        exit('Fin du programme'); // La fct. exit() permet d'arrêter le script.
-    }
-    $reverse = strtolower(strrev($mot));
-    if (strtolower($mot) == $reverse) {
-        echo "Ce mot est un palindrome \n";
-    } else {
-        echo "Ce mot n'est pas un palindrome \n";
+        } elseif ($reponse === 'n') {
+            return false;
+        }
     }
 }
- */
 
-$insultes = ['con', 'merde'];
-$asterisque = [];
-foreach ($insultes as $insulte) {
 
-    $premiereLettre = substr($insulte, 0, 1);
-    $asterisque[] = $premiereLettre . str_repeat('*', strlen($insulte) - 1);
-    
+function demander_creneau ($phrase = 'Veuillez entrer un creneau') {
+    echo $phrase."\n";
+    while (true) {
+        $ouverture = (int)readline("Heure d'ouverture : ");
+        if ($ouverture >= 0 && $ouverture <= 23) {
+            break;
+        }
+    }
+    while (true) {
+        $fermeture = (int)readline("Heure de fermeture : ");
+        if ($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture) {
+            break;
+        }
+    }
+    return [$ouverture, $fermeture];
+        
 }
 
-$phrase = readline("Entrez votre phrase : \n");
-$phrase = strtolower($phrase);
-$phrase = str_replace($insultes, $asterisque, $phrase);
+// $resultat = repondre_oui_non("Voulez-vous continuer?");
+/* $creneau = demander_creneau();
+$creneau2 = demander_creneau("Veuillez entrer votre creneau");
 
-/* foreach ($insultes as $insulte) {
-    $taille = strlen($insulte);
-    $replace = str_repeat('*', $taille);
-    $phrase = str_replace($insulte, $replace, $phrase);
+var_dump($creneau, $creneau2); */
+
+// typage des params.
+// typage de ce que retrourne la fct.
+function demander_creneaux (string $phrase = "Veuillez entrer vos creneaux : "):  array {
+    $creneaux = [];
+    $continuer = true;
+
+    while ($continuer) {
+        $creneaux[] = demander_creneau();
+        $continuer = repondre_oui_non('Voulez-bous continuer? ');
+    }
+    return $creneaux;
 }
-*/
+$creneaux = demander_creneaux ("Entrez vos creneaux");
+var_dump($creneaux);
 
 
+/* 
+function demo (string $param) {
+    var_dump($param);
+}
 
-
-echo $phrase;
+demo(1.2); */
